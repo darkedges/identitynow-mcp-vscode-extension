3280ff019baaa00e8c5c5d976c2b5428261477c5
DarkEdges <nirving.darkedges@darkedges.com>
2025-10-05 12:50:02 +1100

    Updated path to mcp server

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
+import path from 'path';
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
   const didChangeEmitter = new vscode.EventEmitter<void>();
-
   context.subscriptions.push(vscode.lm.registerMcpServerDefinitionProvider('darkedges.identitynow_provider', {
     onDidChangeMcpServerDefinitions: didChangeEmitter.event,
     provideMcpServerDefinitions: async () => {
@@ -10,22 +10,25 @@ export function activate(context: vscode.ExtensionContext) {
       servers.push(new vscode.McpStdioServerDefinition(
         'IdentityNow',
         'node',
-        ['mcp.js'],
+        [
+          path.join(__dirname, '..', 'out', 'mcp.js')
+        ],
         {
           SAILPOINT_BASE_URL: '',
           SAILPOINT_CLIENT_ID: '',
           SAILPOINT_CLIENT_SECRET: ''
         },
-        '1.0.0'
+        '1.0.1'
       ));
 
       return servers;
     },
     resolveMcpServerDefinition: async (server: vscode.McpServerDefinition) => {
 
-      if (server.label === 'IdentityNow Provider') {
-        // Get the API key from the user, e.g. using vscode.window.showInputBox
-        // Update the server definition with the API key
+      if (server.label === 'IdentityNow') {
+        const sailpointBaseUrl = await vscode.window.showInputBox({ prompt: 'Enter IdentityNow Tenant URL' });
+        const sailpointClientId = await vscode.window.showInputBox({ prompt: 'Enter IdentityNow Client Id' });
+        const sailpointClientSecret = await vscode.window.showInputBox({ prompt: 'Enter IdentityNow Client Secret' });
       }
 
       // Return undefined to indicate that the server should not be started or throw an error
@@ -37,4 +40,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
